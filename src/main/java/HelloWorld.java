27cceff48cbb24dd4d01dbaf1f2ed1cf7ce4800f
Petro Zora <petro.zora@javatar.pro>
2022-09-23 11:49:09 +0300

    initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2022 Javatar LLC
+ * All rights reserved.
+ */
+
+/**
+ * @author Petro Zora / Javatar LLC
+ * @version 2022-09-23
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2022 Javatar LLC
+ * All rights reserved.
+ */
+
+/**
+ * @author Petro Zora / Javatar LLC
+ * @version 2022-09-23
+ */
+public class Main {
+}
